bfb3593e9f850a8c40a18d1f3835325c8aa33adb
Nicolas Rosa <nicolasfrancacastrorosa@gmail.com>
2024-11-08 12:15:32 -0300

    Update Meio.jsx
    responsividade

--- src/Components/Meio.jsx
+++ src/Components/Meio.jsx
@@ -1,7 +1,7 @@
 const Meio = ({ texto, titulo }) => {
   return (
     <div className="justify-center mt-10 m-auto h-[40vh] w-[70vw] max-sm:h-[20vh]  rounded-[1.5em] border-[1px] border-[#248497] bg-gradient-to-r from-[#42a0e883] to-[#2e8b561a] text-pretty">
-     <h1 className="justify-center text-center text-[3em] text-ellipsis overflow-hidden whitespace-nowrap max-sm:text-[1.0em] max-md:text-[0.6em]  max-lg:text-[2em]">{titulo}</h1>
+     <h1 className="justify-center text-center text-[3em] text-ellipsis overflow-hidden whitespace-nowrap max-sm:text-[1.0em] max-md:text-[0.6em]  max-lg:text-[2em] font-bold">{titulo}</h1>
       <img
         className="float-left w-auto h-auto max-w-[50%] max-h-[80%] max-sm:h-[70%] ml-4 mt-3 "
         src="https://i.ibb.co/h7TC71S/Ebook-pana.png" 
@@ -9,7 +9,7 @@ const Meio = ({ texto, titulo }) => {
       />
 
       
-      <p className="text-ellipsis overflow-hidden max-h-[80%] ">{texto}</p>
+      <p className="text-ellipsis overflow-hidden max-sm:text-[0.5em] max-md:text-[0.6em]  max-lg:text-[1em] ">{texto}</p>
     </div>
   );
 };
