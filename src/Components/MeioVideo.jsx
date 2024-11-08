eb9b45774c90f5ab4f5b855ab607b244d945afb9
Nicolas Rosa <nicolasfrancacastrorosa@gmail.com>
2024-11-08 13:11:16 -0300

    Update MeioVideo.jsx

--- src/Components/MeioVideo.jsx
+++ src/Components/MeioVideo.jsx
@@ -2,10 +2,7 @@ const MeioVD = ({ texto, titulo, vd }) => {
     return (
       <div className="flex flex-col  items-center justify-center  mt-10 mx-auto h-[70vh] w-full max-sm:mr-0 max-sm:h-[50vh] rounded-[1.5em]  border-[#248497] bg-gradient-to-r from-[#42a0e883] to-[#2e8b561a] text-black">
         <div className="flex flex-row max-sm:flex-col ">
-          <video width="800" controls className="float-left w-[50vw] items-center max-sm:h-auto  max-sm:w-[75vw] ml-4 mt-3 rounded-[12px] drop-shadow-lg">
-            <source src={vd} type="video/mp4"/> {/* Correção aqui */}
-            Seu navegador não suporta a tag de vídeo.
-          </video>
+        <iframe width="708" height="397" src="https://www.youtube.com/embed/0KQ9P1VWn0Y?si=MH_8r__QOtYL8T-Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <div className="ml-8 mb-1">
             <h1 className="text-center text-[1.5em] font-bold max-sm:text-[1.5em]">{titulo}</h1>
             <p className=" max-sm:text-[0.5em]">{texto}</p>
