8a952072a1ae1801f15456353651c28076eaee5d
Nicolas Rosa <nicolasfrancacastrorosa@gmail.com>
2024-11-08 12:06:10 -0300

    Update Meio.jsx

--- src/Components/Meio.jsx
+++ src/Components/Meio.jsx
@@ -4,7 +4,7 @@ const Meio = ({ texto, titulo }) => {
      <h1 className="justify-center text-center text-[3em] text-ellipsis overflow-hidden whitespace-nowrap max-sm:text-[1.0em] max-md:text-[0.6em]  max-lg:text-[2em]">{titulo}</h1>
       <img
         className="float-left w-auto h-auto max-w-[50%] max-h-[80%] max-sm:h-[70%] ml-4 mt-3 "
-        src="src\img\Ebook-pana.svg"
+        src="https://i.ibb.co/h7TC71S/Ebook-pana.png" 
         alt=""
       />
 
